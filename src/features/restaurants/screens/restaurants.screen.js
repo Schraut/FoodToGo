b231d95e24ccfa64c39829d80ad5672aa4b9b17f
Dan Shrout <danshrout1@gmail.com>
2021-02-19 09:35:14 -0800

    fixed statusbar bug, setup theme with colors, fonts, sizes, spacing, and index. refactored code to use theme, installed expo and google fonts, installed react-native-svg, added star and open icons.

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -7,17 +7,16 @@ import { RestaurantInfoCard } from "../components/restaurant-info-card.component
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchBarContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantListContainer = styled.View`
   flex: 1;
-  padding: 16px;
-  background-color: blue;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantsScreen = () => (
