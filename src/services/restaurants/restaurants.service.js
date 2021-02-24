dffa921436ae90a62f1af50b1522a7fa5076d033
Dan Shrout <danshrout1@gmail.com>
2021-02-24 07:46:03 -0800

    hooked up search bar to look up restaurants, installed camelize to make json data camel case, setup context with React, moved search bar into its own component.

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,6 +1,7 @@
-import { mocks } from "./mock";
+import { mocks, mockImages } from "./mock";
+import camelize from "camelize";
 
-export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+export const restaurantsRequest = (location) => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
     if (!mock) {
@@ -9,10 +10,19 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
     resolve(mock);
   });
 };
-restaurantsRequest()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log("error" + err);
+
+export const restaurantsTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    restaurant.photos = restaurant.photos.map((p) => {
+      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
+    });
+
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
   });
+
+  return camelize(mappedResults);
+};
