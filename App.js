bc6da3239cf4f033399f6155ff50ed367f1bc71b
Dan Shrout <danshrout1@gmail.com>
2021-03-04 08:27:14 -0800

    completed register and login screens, moved most of context out of app.js and into the correct files, created a logout function and favorites function inside settings.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { ThemeProvider } from "styled-components/native";
 // Firebase
 import * as firebase from "firebase";
@@ -14,9 +14,6 @@ import { theme } from "./src/infrastructure/theme";
 // Navigation
 import { Navigation } from "./src/infrastructure/navigation";
 // Context
-import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
-import { LocationContextProvider } from "./src/services/location/location.context";
-import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
 
 // Firebase config
@@ -50,13 +47,7 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <AuthenticationContextProvider>
-          <FavouritesContextProvider>
-            <LocationContextProvider>
-              <RestaurantsContextProvider>
-                <Navigation />
-              </RestaurantsContextProvider>
-            </LocationContextProvider>
-          </FavouritesContextProvider>
+          <Navigation />
         </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
