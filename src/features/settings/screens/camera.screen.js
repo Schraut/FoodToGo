4f893f822e04fa7906e71f8d48f769abc17034ba
Dan Shrout <danshrout1@gmail.com>
2021-03-05 10:11:32 -0800

    replaced icon.png with correct dimensions, camera can now take photos and save them to the users profile screen.

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,17 +1,38 @@
-import React, { useRef, useState, useEffect } from "react";
-import { Camera } from "expo-camera";
+import React, { useRef, useState, useEffect, useContext } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
-import { View } from "react-native";
+import { Camera } from "expo-camera";
+
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
 import { Text } from "../../../components/typography/text.component";
 
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
+  flex: 1;
 `;
 
-export const CameraScreen = () => {
+const InnerSnap = styled.View`
+  width: 100%;
+  height: 100%;
+  z-index: 999;
+`;
+
+export const CameraScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const cameraRef = useRef();
+  const { user } = useContext(AuthenticationContext);
+
+  const snap = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log(photo);
+      AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+      navigation.goBack();
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -30,6 +51,11 @@ export const CameraScreen = () => {
     <ProfileCamera
       ref={(camera) => (cameraRef.current = camera)}
       type={Camera.Constants.Type.front}
-    ></ProfileCamera>
+      ratio={"16:9"}
+    >
+      <TouchableOpacity onPress={snap}>
+        <InnerSnap />
+      </TouchableOpacity>
+    </ProfileCamera>
   );
 };
